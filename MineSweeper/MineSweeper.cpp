62c0c5ee1f7500fab86777706880e54ea0af1613
Andrey Chernov <Andrey.Chernov@lanit-tercom.com>
2019-12-12 15:55:05 +0300

    upd

--- MineSweeper/MineSweeper.cpp
+++ MineSweeper/MineSweeper.cpp
@@ -36,6 +36,33 @@ std::vector<std::vector<int>> parseFile(std::string name)
 	return result;
 }
 
+void printMap(std::vector<std::vector<int>> map)
+{
+	for (int i = 0; i < map.size(); i++)
+	{
+		for (int j = 0; j < map[i].size(); j++)
+		{
+			switch (map[i][j])
+			{
+			case -1:
+				std::cout << "*\t";
+				break;
+			case 9:
+				std::cout << "m\t";
+				break;
+			case 10:
+				std::cout << "E\t";
+				break;
+			default:
+				std::cout << map[i][j] << "\t";
+				break;
+			}
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+}
+
 void countNeighbours(int i, int j, std::vector<std::vector<int>> map, int &mines, int &undefined)
 {
 	mines = 0;
@@ -54,77 +81,6 @@ void countNeighbours(int i, int j, std::vector<std::vector<int>> map, int &mines
 			}
 		}
 	}
-}
-
-int cellCheck(std::vector<std::vector<int>> map)
-{
-	for (int i = 1; i < 4; i++)
-	{
-		for (int j = 1; j < 4; j++)
-		{
-			if ((map[i][j] >= 0) && (map[i][j] <= 8))
-			{
-				int mines, undefined;
-				countNeighbours(i, j, map, mines, undefined);
-				if (mines == map[i][j])
-				{
-					return 10;
-				}
-				else if ((mines + undefined) == map[i][j])
-				{
-					return 9;
-				}
-			}
-		}
-	}
-	return -1;
-}
-
-bool singleRun(std::vector<std::vector<int>> &map)
-{
-	bool updated = false;
-	for (int i = 0; i < map.size(); i++)
-	{
-		for (int j = 0; j < map[i].size(); j++)
-		{
-			if (map[i][j] < 0)
-			{
-				std::vector<std::vector<int>> window;
-				for (int y = i - 2; y <= i + 2; y++)
-				{
-					std::vector<int> line;
-					if ((y < 0) || (y >= map.size()))
-					{
-						for (int k = 0; k < 5; k++)
-						{
-							line.push_back(-2);
-						}
-					}
-					else
-					{
-						for (int x = j - 2; x <= j + 2; x++)
-						{
-							if ((x < 0) || (x >= map[i].size()))
-							{
-								line.push_back(-2);
-							}
-							else
-							{
-								line.push_back(map[y][x]);
-							}
-						}
-					}
-					window.push_back(line);
-				}
-				map[i][j] = cellCheck(window);
-				if (map[i][j] != -1)
-				{
-					updated = true;
-				}
-			}
-		}
-	}
-	return updated;
 }
 
 bool consistencyCheck(std::vector<std::vector<int>> map)
@@ -178,44 +134,177 @@ bool consistencyCheck(std::vector<std::vector<int>> map)
 	return true;
 }
 
-void printMap(std::vector<std::vector<int>> map)
+bool countCheck(std::vector<std::vector<int>> map)
 {
+	int mines = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (map[i][j] == 9)
+			{
+				mines++;
+			}
+		}
+	}
+	return (mines == map[1][1]);
+}
+
+bool canBeSolved(std::vector<std::vector<int>> map, int vars)
+{
+	if (vars != 0)
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			for (int j = 0; j < 7; j++)
+			{
+				if (map[i][j] == -1)
+				{
+					map[i][j] = 9;
+					if (canBeSolved(map, vars - 1))
+					{
+						return true;
+					}
+					map[i][j] = 10;
+					return canBeSolved(map, vars - 1);
+				}
+			}
+		}
+	}
+	else
+	{
+		for (int i = 1; i <= 5; i++)
+		{
+			for (int j = 1; j <= 5; j++)
+			{
+				if (map[i][j] != 9 && map[i][j] != -2 && map[i][j] != 10)
+				{
+					std::vector<std::vector<int>> window;
+					for (int y = i - 1; y <= i + 1; y++)
+					{
+						std::vector<int> line;
+						for (int x = j - 1; x <= j + 1; x++)
+						{
+							line.push_back(map[y][x]);
+						}
+						window.push_back(line);
+					}
+					if (!countCheck(window))
+					{
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+}
+
+int cellCheck(std::vector<std::vector<int>> map)
+{
+	int undefined = -1;
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			if (map[i][j] == -1)
+			{
+				undefined++;
+			}
+		}
+	}
+	map[3][3] = 9;
+	bool canBeMine = canBeSolved(map, undefined);
+	map[3][3] = 10;
+	bool canBeEmpty = canBeSolved(map, undefined);
+	if (canBeEmpty && !canBeMine)
+	{
+		return 10;
+	}
+	else if (canBeMine && !canBeEmpty)
+	{
+		return 9;
+	}
+	else if (canBeMine && canBeEmpty)
+	{
+		return -1;
+	}
+	else
+	{
+		return -3;
+	}
+}
+
+bool singleRun(std::vector<std::vector<int>> &map, bool &inconsistent)
+{
+	inconsistent = false;
+	bool updated = false;
 	for (int i = 0; i < map.size(); i++)
 	{
 		for (int j = 0; j < map[i].size(); j++)
 		{
-			switch (map[i][j])
+			if (map[i][j] < 0)
 			{
-			case -1:
-				std::cout << "*\t";
-				break;
-			case 9:
-				std::cout << "m\t";
-				break;
-			case 10:
-				std::cout << "E\t";
-				break;
-			default:
-				std::cout << map[i][j] << "\t";
-				break;
+				std::vector<std::vector<int>> window;
+				for (int y = i - 3; y <= i + 3; y++)
+				{
+					std::vector<int> line;
+					if ((y < 0) || (y >= map.size()))
+					{
+						for (int k = 0; k < 7; k++)
+						{
+							line.push_back(-2);
+						}
+					}
+					else
+					{
+						for (int x = j - 3; x <= j + 3; x++)
+						{
+							if ((x < 0) || (x >= map[i].size()))
+							{
+								line.push_back(-2);
+							}
+							else
+							{
+								line.push_back(map[y][x]);
+							}
+						}
+					}
+					window.push_back(line);
+				}
+				map[i][j] = cellCheck(window);
+				if (map[i][j] == -3)
+				{
+					inconsistent = true;
+					return false;
+				}
+				else if (map[i][j] != -1)
+				{
+					updated = true;
+				}
 			}
 		}
-		std::cout << "\n";
 	}
-	std::cout << "\n";
+	return updated;
 }
 
 int main(int argc, char *argv[])
 {
 	std::vector<std::vector<int>> map = parseFile(argv[1]);
 	printMap(map);
-	do
+	if (!consistencyCheck(map))
 	{
-		if (!consistencyCheck(map))
-		{
-			std::cout << "Map is inconsistent\n";
-			return 0;
-		}
-	} while (singleRun(map));
-	printMap(map);
+		std::cout << "Map is inconsistent\n";
+		return 0;
+	}
+	bool inconsistency;
+	while (singleRun(map, inconsistency));
+	if (inconsistency)
+	{
+		std::cout << "Map is inconsistent\n";
+	}
+	else
+	{
+		printMap(map);
+	}
 }
